551acb9431d20357428b83d5834a8fb4d0db3625
octohmy <m.hawes93@gmail.com>
2023-04-09 12:21:26 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,25 @@
+
 /* 
-👉 Write your kata here!
+
+REMAINING CARD COUNTER ♥️♠️♦️♣️
+Instructions:
+- A deck of cards has 4 cards of each value (4 Aces, 4 Jacks... etc.)
+- When you input a string of letters and numbers of all the cards picked ("6,7,8,A")
+
+- Write a program that returns the remaining cards, giving the face value and how many cards of that type remain
+
+- For example, the return of remainingCards("6,7,8,A") should return "2:4,3:4,4:4,5:4,6:3,7:3,8:3,9:4,10:4,J:4,Q:4,K:4,A:3"
+- ^ Card values of 6, 7, 8, A are now equal to 3
+
+- Account for edge cases and if nobody is willing to play, tell them to "Go home and come back later with cash" ;) 
+
+have fun
+
 */
 
-//👉 Write the function your CodeWarriors will start with below here:
+export function remainingCards(str){
+      //write your answer here
+}
+
+
+// with thanks to: Ahia, Chris, Safiyah, Michael
